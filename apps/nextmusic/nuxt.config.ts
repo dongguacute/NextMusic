9e7daa0488859a1141b1858747be4300dd668cce
dongguacute <gudupao2022@gmail.com>
2026-04-08 11:58:38 +0800

    feat(config): configure devServer to allow external access
    - Added devServer configuration with host set to '0.0.0.0' to enable external access during development.
    - This change facilitates easier testing and collaboration in local development environments.

--- apps/nextmusic/nuxt.config.ts
+++ apps/nextmusic/nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  devServer: {
+    host: '0.0.0.0'
+  },
   future: {
     compatibilityVersion: 4,
   },
